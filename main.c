0ced72e3681c1160f7d3b09170b406370d90f4cd
Allan Gilmore <allangilmoreone@gmail.com>
2018-03-19 19:57:34 -0700

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: allangilmore
+ *
+ * Created on March 19, 2018, 7:53 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
